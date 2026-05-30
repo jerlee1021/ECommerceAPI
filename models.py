87ae0e9fbf1aabae0460bfd399617c3ee18eb233
Jeremy Lee <jeremy10@illinois.edu>
2026-05-30 13:39:50 -0700

    Finished Models for Table

--- models.py
+++ models.py
@@ -7,3 +7,27 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(10), nullable=False, default="user")
     created_at = db.Column(db.DateTime, default=db.func.now())
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+class CartItem(db.Model):
+    __tablename__ = "cart_items"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+class Order(db.Model):
+    __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    total = db.Column(db.Numeric(10, 2), nullable=False)
+    status = db.Column(db.String(120), nullable=False, default="pending")
+    stripe_payment_id = db.Column(db.String(120), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
