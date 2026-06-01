51bf3546d474451128c276f981b0a5c6120a160f
Jeremy Lee <jeremy10@illinois.edu>
2026-06-01 14:19:56 -0700

    Finshed register route

--- auth.py
+++ auth.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
 from extensions import db, bcrypt
 from models import User
+import jwt
+import datetime
+import os
 
 auth = Blueprint("auth", __name__)
 
@@ -25,3 +28,23 @@ def register():
     db.session.commit()
 
     return jsonify({"message": "user created"}), 201
+
+@auth.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    if(not email or not password):
+        return jsonify({"error": "email and password are required"}), 400
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if not user or not bcrypt.check_password_hash(user.password_hash, password):
+        return jsonify({"error": "Invalid Credentials"}), 401
+    
+    payload = {"user_id": user.id, "role": user.role, "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=24)}
+
+    encoded_jwt = jwt.encode(payload, os.environ.get("SECRET_KEY"), algorithm="HS256")
+
+    return jsonify({"token": encoded_jwt}), 200
+
